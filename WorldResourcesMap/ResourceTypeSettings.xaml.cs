eaa88d3f1d9268da5a4dfa31d68286f66751a44b
Boris Sulicenko <b.sulicenko@gmail.com>
2019-05-30 23:49:12 +0200

    Uradjena je izmena tipa resursa. Sitne izmene.

--- WorldResourcesMap/ResourceTypeSettings.xaml.cs
+++ WorldResourcesMap/ResourceTypeSettings.xaml.cs
@@ -51,16 +51,41 @@ namespace WorldResourcesMap
 
             InitializeComponent();
 
-            this.manager.MapData.Types.Add(new ResourceType(1, "aa", "", "opis1"));
-            this.manager.MapData.Types.Add(new ResourceType(2, "bb", "", "opis2"));
-            this.manager.MapData.Types.Add(new ResourceType(3, "cc", "", "opis3"));
+            EnableEditForm(false);
+
+            this.manager.MapData.Types.Add(new ResourceType(1, "aa", @"E:\dev\WorldResourcesMap\WorldResourcesMap\resources\images\no-image.png", "opis1"));
+            this.manager.MapData.Types.Add(new ResourceType(2, "bb", @"E:\dev\WorldResourcesMap\WorldResourcesMap\resources\images\no-image.png", "opis2"));
+            this.manager.MapData.Types.Add(new ResourceType(3, "cc", @"E:\dev\WorldResourcesMap\WorldResourcesMap\resources\images\no-image.png", "opis3"));
 
             View = CollectionViewSource.GetDefaultView(this.manager.MapData.Types);
         }
 
+        private void EnableEditForm(bool state)
+        {
+            txtBoxId.IsEnabled = state;
+            txtBoxName.IsEnabled = state;
+            txtBoxDescription.IsEnabled = state;
+            btnChangeImage.IsEnabled = state;
+            btnRemoveImage.IsEnabled = state;
+            btnEditType.IsEnabled = state;
+            btnRemoveType.IsEnabled = state;
+        }
+
         private void EditItem(object sender, RoutedEventArgs e)
         {
+            ResourceType item = dgrMain.SelectedItem as ResourceType;
 
+            if (MessageBox.Show("Da li ste sigurni da želite da izmenite podatake za tip resursa sa oznakom " + item.Id + " ?",
+                "Upozorenje o izmeni podataka", MessageBoxButton.YesNo,
+                MessageBoxImage.Warning) == MessageBoxResult.No)
+            {
+                return;
+            }
+
+            item.Id = int.Parse(txtBoxId.Text);
+            item.Name = txtBoxName.Text;
+            item.Description = txtBoxDescription.Text;
+            item.Icon = resTypeImage.Source.ToString();
         }
 
         private void DeleteItem(object sender, RoutedEventArgs e)
@@ -88,8 +113,7 @@ namespace WorldResourcesMap
 
             if (fileDialog.ShowDialog() == true)
             {
-                ResourceType item = dgrMain.SelectedItem as ResourceType;
-                item.Icon = fileDialog.FileName;
+                resTypeImage.Source = new BitmapImage(new Uri(fileDialog.FileName));
             }
         }
 
@@ -97,6 +121,21 @@ namespace WorldResourcesMap
         {
             ResourceType item = dgrMain.SelectedItem as ResourceType;
             item.Icon = "./resources/images/no-image.png";
+        }
+
+        private void DataGridSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            EnableEditForm(true);
+
+            ResourceType item = dgrMain.SelectedItem as ResourceType;
+
+            if (txtBoxId == null) // Komponenta jos nije inicijalizovana
+                return;
+
+            txtBoxId.Text = item.Id.ToString();
+            txtBoxName.Text = item.Name;
+            txtBoxDescription.Text = item.Description;
+            resTypeImage.Source = new BitmapImage(new Uri(item.Icon));
         }
     }
 }
